6d73074486c0618c255993846f3bb1a8ac8a78a0
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-06-10 23:38:40 +0200

    Let user to input address

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const request = require('request-promise');
+const yargs = require('yargs');
 
 const apiKey = 'AIzaSyCCPCftNRWGuwXoRcLzQoYvPm9S9NwwjIk';
-const url =
-    'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia';
 
-const location = request({
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        },
+    })
+    .help()
+    .alias('h', 'help').argv;
+
+const encodedAddress = encodeURI(argv.a);
+const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+const locationOptions = {
     url: `${url}&${apiKey}`,
     json: true,
-}).then(response => console.log(response));
+};
+
+const location = request(locationOptions)
+    .then(response => {
+        console.log(`Address: ${response.results[0].formatted_address}`);
+        console.log(`Latitude is ${response.results[0].geometry.location.lat}`);
+        console.log(`Longitude is ${response.results[0].geometry.location.lng}`);
+    })
+    .catch(err => console.log(JSON.stringify(err, null, 2)));
