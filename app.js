f00e84e1691a2406e1e31459c2501fad8e5be12b
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-06-11 22:34:01 +0200

    Add geocode file with geocodeAddress  function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const request = require('request-promise');
 const yargs = require('yargs');
 
-const apiKey = 'AIzaSyCCPCftNRWGuwXoRcLzQoYvPm9S9NwwjIk';
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
     .options({
@@ -15,22 +14,4 @@ const argv = yargs
     .help()
     .alias('h', 'help').argv;
 
-const encodedAddress = encodeURI(argv.a);
-const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-
-const locationOptions = {
-    url: `${url}&key=${apiKey}`,
-    json: true,
-};
-
-const location = request(locationOptions)
-    .then(response => {
-        if (response.status === 'ZERO_RESULTS') {
-            return console.log('Unable to find that address.');
-        }
-
-        console.log(`Address: ${response.results[0].formatted_address}`);
-        console.log(`Latitude is ${response.results[0].geometry.location.lat}`);
-        console.log(`Longitude is ${response.results[0].geometry.location.lng}`);
-    })
-    .catch(error => console.log('Unable to connect to Google servers.'));
+geocode.geocodeAddress(argv.address);
---
+++ geocode/geocode.js
@@ -0,0 +1,28 @@
+const request = require('request-promise');
+
+const geocodeAddress = address => {
+    const encodedAddress = encodeURI(address);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+    const apiKey = 'AIzaSyCCPCftNRWGuwXoRcLzQoYvPm9S9NwwjIk';
+
+    const locationOptions = {
+        url: `${url}&key=${apiKey}`,
+        json: true,
+    };
+
+    return request(locationOptions)
+        .then(response => {
+            if (response.status === 'ZERO_RESULTS') {
+                return console.log('Unable to find that address.');
+            }
+
+            console.log(`Address: ${response.results[0].formatted_address}`);
+            console.log(`Latitude is ${response.results[0].geometry.location.lat}`);
+            console.log(`Longitude is ${response.results[0].geometry.location.lng}`);
+        })
+        .catch(() => console.log('Unable to connect to Google servers.'));
+};
+
+module.exports = {
+    geocodeAddress,
+};
